74896a4b5d509f7b0c8e08caaceeafaf3788ff05
allan.pirillis <allan.p94@gmail.com>
2021-03-28 21:59:08 -0500

    added functionality for the quiz end; data is stored and retrieved from local storage and displayed as a list of high scores; page is refreshed to it's initial state;

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,54 @@ var index = 0;
 var timeLeft = 60;
 var questionBodyEl = document.querySelector(".question-body");
 var countdownEl = document.getElementById("countdown");
+var highScoreEl = document.querySelector(".highscores-btn");
+var submitInitialsEl = document.querySelector(".btn submit-btn");
+var containerEl = document.querySelector(".container");
+localStorage.setItem("page_html", containerEl.innerHTML);
+
+// if local storage has an item name users it sets the array with that info else empty
+var users = JSON.parse(localStorage.getItem("users")) || [];
+
+// initializing my setTimer variables globaly so i can clear them later
+var countDown;
+var delayNextQuestion;
 
 function startTimer() {
-  var countDown = setInterval(function () {
+  countDown = setInterval(function () {
     if (timeLeft > 0) {
       countdownEl.innerText = "TIME: " + timeLeft;
       timeLeft--;
     } else if (timeLeft === 0) {
       //call a function that the quiz is over and display the score
       countdownEl.innerText = "TIME: 0";
+      endQuiz();
     }
   }, 1000);
 }
 
 function createQuestion(questIndex) {
-  //clear the existiing content
-  questionBodyEl.innerHTML = "";
+  if (questIndex === questions.length) {
+    endQuiz();
+  } else {
+    //clear the existiing content
+    questionBodyEl.innerHTML = "";
 
-  //create the question head
-  var questionHeadEl = document.createElement("h2");
-  questionHeadEl.className = "question-head";
-  questionHeadEl.textContent = questions[index].question;
-  questionBodyEl.appendChild(questionHeadEl);
+    //create the question head
+    var questionHeadEl = document.createElement("h2");
+    questionHeadEl.className = "question-head";
+    questionHeadEl.textContent = questions[index].question;
+    questionBodyEl.appendChild(questionHeadEl);
 
-  // create the answer options for the question based on the index
-  var answers = questions[questIndex].answers;
-  for (var i = 0; i < answers.length; i++) {
-    var buttonEl = document.createElement("button");
-    buttonEl.className = "btn question-btn";
-    buttonEl.setAttribute("index", questIndex);
-    buttonEl.textContent = questions[questIndex].answers[i];
-    questionBodyEl.appendChild(buttonEl);
+    // create the answer options for the question based on the index
+    var answers = questions[questIndex].answers;
+    for (var i = 0; i < answers.length; i++) {
+      var buttonEl = document.createElement("button");
+      buttonEl.className = "btn question-btn";
+      buttonEl.setAttribute("index", questIndex);
+      buttonEl.textContent = questions[questIndex].answers[i];
+      questionBodyEl.appendChild(buttonEl);
+    }
+    index++;
   }
 }
 
@@ -53,41 +70,158 @@ function displayAnswer(bool) {
   questionBodyEl.appendChild(textEl);
 }
 
-function checkAnswer(button) {
-  var buttonIndex = button.textContent;
-  if (buttonIndex === questions[index - 1].correctAnswer) {
-    console.log("add 10 seconds to the timer");
+function timerHandle(a) {
+  if (a === 10) {
     timeLeft += 10;
+  } else if (a === -10) {
+    if (timeLeft > 10) {
+      timeLeft -= 10;
+    } else {
+      endQuiz();
+    }
+  }
+}
+
+function checkAnswer(button) {
+  var buttonText = button.textContent;
+  if (buttonText === questions[index - 1].correctAnswer) {
+    timerHandle(10);
     displayAnswer(true);
   } else {
-    console.log("deduct 10 seconds to the timer");
-    timeLeft -= 10;
+    timerHandle(-10);
     displayAnswer(false);
   }
 }
 
 function buttonHandler(event) {
   //if the start button is clicked it does not check if the answer is correct
+
   if (event.target.matches(".start-btn")) {
     createQuestion(index);
-    startTimer(0);
-    index++;
+    startTimer();
 
     // if the event.target matches the classname .question then the user clicked the button
   } else if (event.target.matches(".question-btn")) {
     checkAnswer(event.target);
     //delay the next answer for 1s
-    setTimeout(function () {
+    delayNextQuestion = setTimeout(function () {
       createQuestion(index);
-    }, 1500);
-
-    if (index > questions.length - 1) {
-      alert("call the end function");
-      //call the end function
-    } else {
-      index++;
-    }
+    }, 1200);
   }
 }
 
-questionBodyEl.addEventListener("click", buttonHandler);
+function endQuiz() {
+  //clearing the setTimeouts so they dont interfiar with the logic
+  clearInterval(countDown);
+  clearInterval(delayNextQuestion);
+  //clear the existiing content
+  questionBodyEl.innerHTML = "";
+  countdownEl.innerText = "TIME: 0";
+
+  var questionHeadEl = document.createElement("h2");
+  questionHeadEl.className = "question-head";
+
+  var scoreEl = document.createElement("p");
+  scoreEl.className = "result-text";
+
+  //display the score
+  if (timeLeft > 0) {
+    questionHeadEl.textContent = "ALL DONE -- LET'S SEE HOW YOU DID";
+    scoreEl.textContent = "Your final score is " + timeLeft;
+  } else if (timeLeft === 0) {
+    questionHeadEl.textContent = "Ooops -- you ran out of time!!";
+    scoreEl.textContent = "Your final score is " + timeLeft;
+  }
+  questionBodyEl.appendChild(questionHeadEl);
+  questionBodyEl.appendChild(scoreEl);
+
+  var userInfoEl = document.createElement("div");
+  userInfoEl.className = "user-info";
+
+  var userTextInfoEl = document.createElement("p");
+  userTextInfoEl.className = "highscore-text";
+  userTextInfoEl.innerHTML = "Enter Initials: ";
+  userInfoEl.appendChild(userTextInfoEl);
+
+  var userInputEl = document.createElement("input");
+  userInputEl.className = "input";
+  userInfoEl.appendChild(userInputEl);
+
+  var submitButtonEl = document.createElement("button");
+  submitButtonEl.className = "btn submit-btn";
+  submitButtonEl.innerHTML = "Submit";
+  userInfoEl.appendChild(submitButtonEl);
+
+  questionBodyEl.appendChild(userInfoEl);
+}
+
+function viewHighscores(users) {
+  highScoreEl.innerHTML = "";
+  document.querySelector(".topbar").innerHTML = "";
+  questionBodyEl.innerHTML = "";
+
+  var highscoreDivEl = document.createElement("div");
+  highscoreDivEl.className = "highscore-container";
+
+  var highscoreTextEl = document.createElement("h2");
+  highscoreTextEl.className = "question-head";
+  highscoreTextEl.textContent = "High Scores";
+  highscoreDivEl.appendChild(highscoreTextEl);
+
+  var highscoreUlEl = document.createElement("ul");
+  highscoreUlEl.className = "highscore-ul";
+  highscoreDivEl.appendChild(highscoreUlEl);
+
+  for (var i = 0; i < users.length; i++) {
+    var highscoreLiEl = document.createElement("li");
+    highscoreLiEl.innerHTML =
+      i + 1 + ". " + users[i].name + " -- " + users[i].score;
+    highscoreUlEl.appendChild(highscoreLiEl);
+  }
+
+  var backButtonEl = document.createElement("button");
+  backButtonEl.className = "btn back-btn";
+  backButtonEl.innerHTML = "Go back";
+  highscoreDivEl.appendChild(backButtonEl);
+
+  var clearButtonEl = document.createElement("button");
+  clearButtonEl.className = "btn clear-btn";
+  clearButtonEl.innerHTML = "Clear high scores";
+  highscoreDivEl.appendChild(clearButtonEl);
+
+  questionBodyEl.appendChild(highscoreDivEl);
+}
+
+questionBodyEl.addEventListener("click", function (event) {
+  buttonHandler(event);
+});
+highScoreEl.addEventListener("click", function () {
+  viewHighscores(users);
+});
+
+function saveInitials() {
+  var userInput = document.querySelector("input").value;
+  var userObj = {
+    name: userInput,
+    score: timeLeft,
+  };
+  users.push(userObj);
+  userInput.innerHTML = "";
+
+  localStorage.setItem("users", JSON.stringify(users));
+  viewHighscores(users);
+}
+
+//adding an event listener on a future element
+questionBodyEl.addEventListener("click", function (event) {
+  if (event.target.className === "btn submit-btn") {
+    saveInitials();
+  } else if (event.target.className === "btn back-btn") {
+    history.go(0);
+  } else if (event.target.className === "btn clear-btn") {
+    localStorage.removeItem("users");
+    users = [];
+    viewHighscores(users);
+  } else {
+  }
+});
